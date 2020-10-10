2dd45f2d0ddb32511b03150c8383d57e29afccf0
Oleg Nagaitsev <onagaitsev@yandex.ru>
2020-10-10 20:25:41 +0300

    The first task is finished

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,25 @@
 fun main() {
+    val theFirstComments = Comments (
+            canPost = true
+    )
+    val theFirstLikes = Likes (
+            canLike = true
+    )
+    val theFirstReposts = Reposts ()
+    val theFirstViewsObject = ViewsObject (
+            count = 1
+    )
+
+    val theFirstPost = Post (
+            id = 1,
+            comments = theFirstComments,
+            likes = theFirstLikes,
+            reposts = theFirstReposts,
+            viewsObject = theFirstViewsObject,
+            postType = PostType.POST
+    )
+
+    println(theFirstPost)
+    WallService.add(theFirstPost)
     
 }
